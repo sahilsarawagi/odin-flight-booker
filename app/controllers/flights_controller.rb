f259211a920fac2087516fdb5e13dffbdf70663a
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-02-19 18:54:25 +0530

    fetch matching flights for query from the database. Displayed results in the app/views/flights/index.html.erb view.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,22 @@
 class FlightsController < ApplicationController
   def index
     @airports =Airport.all
-    @flights =Flight.all
+    @flights =Flight.where.not(departure_date: nil).distinct.order(departure_date: :desc)
+    @searched_flights = search_flights
+  end
+
+  def search_flights
+    departure_date = params[:departure_date]
+    arrival_airport_id = Airport.find_by(airport_code: params[:arrival_code])&.id
+    departure_airport_id = Airport.find_by(airport_code: params[:departure_code])&.id
+
+    return [] unless departure_date && arrival_airport_id && departure_airport_id
+
+    Flight.where(departure_date: departure_date, arrival_airport_id: arrival_airport_id, departure_airport_id: departure_airport_id)
+
+  end
+  private
+  def flight_params
+    params.permit(:arrival_code,:departure_code,:num_tickets,:departure_date)
   end
 end
